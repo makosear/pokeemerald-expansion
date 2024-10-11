d11f85b4387a780ec24d5a2d78f0e7734fad2ba3
makosear <80222853+makosear@users.noreply.github.com>
2024-10-11 01:55:09 -0300

    i will prob discard all of this lol

--- src/toggle_map_name_popup.c
+++ src/toggle_map_name_popup.c
@@ -12,47 +12,7 @@
 
 void ToggleMapPopup(void)
 {
-    if (gSaveBlock1Ptr->isPopupActive == 1)
-    {
-        gSaveBlock1Ptr->isPopupActive = 0;
-
-        #ifdef UBFIX
-        if (GetMapNamePopUpWindowId() != WINDOW_NONE)
-    #endif // UBFIX
-        {
-            ClearStdWindowAndFrame(GetMapNamePopUpWindowId(), TRUE);
-            RemoveMapNamePopUpWindow();
-        }
-
-        if (OW_POPUP_GENERATION == GEN_5)
-        {
-            if (GetSecondaryPopUpWindowId() != WINDOW_NONE)
-            {
-                ClearStdWindowAndFrame(GetSecondaryPopUpWindowId(), TRUE);
-                RemoveSecondaryPopUpWindow();
-            }
-
-            DisableInterrupts(INTR_FLAG_HBLANK);
-            SetHBlankCallback(NULL);
-
-            if (OW_POPUP_BW_ALPHA_BLEND && !IsWeatherAlphaBlend())
-            {
-                SetGpuReg(REG_OFFSET_WININ, WININ_WIN0_BG_ALL | WININ_WIN0_OBJ | WININ_WIN1_BG_ALL | WININ_WIN1_OBJ);
-                SetGpuReg(REG_OFFSET_BLDCNT, BLDCNT_TGT2_BG1 | BLDCNT_TGT2_BG2 | BLDCNT_TGT2_BG3 | BLDCNT_TGT2_OBJ | BLDCNT_EFFECT_BLEND);
-                SetGpuReg(REG_OFFSET_BLDALPHA, BLDALPHA_BLEND(8, 10));
-            }
-        }
-    }
-    else
-    {
-        gSaveBlock1Ptr->isPopupActive = 1;
-        ShowMapNamePopUpWindow();
-        if (OW_POPUP_GENERATION == GEN_5)
-            {
-                EnableInterrupts(INTR_FLAG_HBLANK);
-                SetHBlankCallback(HBlankCB_DoublePopupWindow);
-            }
-    }
+    ShowMapNamePopup();
 }
 
 #include "global.h"
