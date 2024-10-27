31b33e30e492f1d7e9549044e70fa11122009beb
makosear <80222853+makosear@users.noreply.github.com>
2024-10-27 12:02:38 -0300

    WIP of intro scene

--- src/new_game.c
+++ src/new_game.c
@@ -128,7 +128,7 @@ static void ClearFrontierRecord(void)
 static void WarpToTruck(void)
 {
     //SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1); //TODO: ENABLE INTRO
-    SetWarpDestination(MAP_GROUP(PELIPPER_TOWN_FARMERS_HOUSE), MAP_NUM(PELIPPER_TOWN_FARMERS_HOUSE), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(PELIPPER_TOWN_FARM_HOUSE_UNPACKED), MAP_NUM(PELIPPER_TOWN_FARM_HOUSE_UNPACKED), WARP_ID_NONE, -1, -1);
     WarpIntoMap();
 }
 
