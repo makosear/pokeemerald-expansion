49bb83781d2f88a23f9a4a591d4ca9f837403746
makosear <80222853+makosear@users.noreply.github.com>
2024-11-09 17:09:47 -0300

    Added Battle BGM change upon the last mon slide in

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -10186,6 +10186,7 @@ static void Cmd_various(void)
             gBattleScripting.battler = battler;
             BattleScriptPush(cmd->nextInstr);
             gBattlescriptCurrInstr = (i == 1 ? BattleScript_TrainerASlideMsgRet : BattleScript_TrainerBSlideMsgRet);
+            SetLastPokemonMusic();
             return;
         }
         break;
